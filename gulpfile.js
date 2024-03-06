191a3f8030cb9b4ee870178d89245947defd6089
iMode79 <ricardo.labrada@me.com>
2024-03-05 18:50:18 -0600

    mejoraImg

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-const { src, dest, watch} = require('gulp');
+//Librerias
+
+const { src, dest, watch, parallel} = require('gulp');
 const sass = require ('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+//Librerias Img
+const cache = require ('gulp-cache');
+const webp = require ('gulp-webp');
+const imagemin = require ('gulp-imagemin');
+const avif = require ('gulp-avif');
+
+
+//Funciones o Tareas
 
 function css (done) {
     src('src/scss/**/*.scss')
@@ -9,7 +19,36 @@ function css (done) {
         .pipe(dest('build/css'));
     done();
 }
-
+//Funcion para WebP
+function versionWebp (done) {
+    const opciones = {
+        quality: 50
+    };
+    src ('src/img/**/*.{png,jpg}')
+    .pipe ( webp ( opciones ) )
+    .pipe ( dest ( 'build/img' ) )
+    done();
+}
+//Funcion para imageMin
+function imagenes (done) {
+    const opciones = {
+        optimizationLevel: 3
+    };
+    src ('src/img/**/*.{png,jpg}')
+    .pipe ( cache ( imagemin ( opciones ) ) )
+    .pipe ( dest ( 'build/img' ) )
+    done();
+}
+//Funcion para Avif
+function versionAvif (done) {
+    const opciones = {
+        quality: 50
+    };
+    src ('src/img/**/*.{png,jpg}')
+    .pipe ( avif (opciones ) )
+    .pipe ( dest ( 'build/img' ) )
+    done();
+}
 
 function dev(done) {
     watch('src/scss/**/*.scss', css)
@@ -17,6 +56,11 @@ function dev(done) {
     done();
 }
 
+//Mandar llamar las funciones
+
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.imagenes = imagenes;
+exports.versionAvif = versionAvif;
+exports.dev =  parallel(versionWebp, imagenes, versionAvif, dev);
 
