f1a6510eac71147bc5786dc35e0d083fa31a387b
iMode79 <ricardo.labrada@me.com>
2024-03-07 17:29:11 -0600

    CommitFinal

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-//Librerias
-
+//Librerias CSS
 const { src, dest, watch, parallel} = require('gulp');
+//CSS
 const sass = require ('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require ('cssnano');
+const postcss = require ('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
 //Librerias Img
 const cache = require ('gulp-cache');
 const webp = require ('gulp-webp');
@@ -11,12 +16,16 @@ const avif = require ('gulp-avif');
 
 
 //Funciones o Tareas
-
+//Funcion para librerias CSS
 function css (done) {
     src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass())
-        .pipe(dest('build/css'));
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'))
     done();
 }
 //Funcion para WebP
@@ -49,18 +58,26 @@ function versionAvif (done) {
     .pipe ( dest ( 'build/img' ) )
     done();
 }
-
+//Funcion para JavaScript
+function javaScript (done) {
+    src ('src/js/*.js')
+    .pipe ( dest ( 'build/js' ) );
+    done();
+}
+//Funcion para el dev del gulpfile (mantener la compilación escuchando)
 function dev(done) {
     watch('src/scss/**/*.scss', css)
-
+    watch('src/js/*.js', javaScript)
     done();
 }
 
-//Mandar llamar las funciones
 
+
+//Mandar llamar las funciones
 exports.css = css;
 exports.versionWebp = versionWebp;
 exports.imagenes = imagenes;
 exports.versionAvif = versionAvif;
-exports.dev =  parallel(versionWebp, imagenes, versionAvif, dev);
+exports.javaScript = javaScript;
+exports.dev =  parallel(css, versionWebp, imagenes, versionAvif, javaScript, dev);
 
